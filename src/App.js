60b4ccd2694b412eca137c218e20d91fcb74c5bc
Josh Rehanek <joshrehanek@gmail.com>
2021-05-08 23:18:49 -0700

    uses spread operator to pass movie prop

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
-const basicCall = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1`
+const basicCall = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 
 const apiSearch = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=Alien`
-
-const imageCall = 'https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg'
-
 
 
 function App() {
@@ -28,7 +25,7 @@ function App() {
   return (
     <div>
       {movies.length > 0 && movies.map((movie) => 
-        <Movie key={movie.id} data={movie}/>
+        <Movie key={movie.id} {...movie}/>
       )}
     </div>
   )
