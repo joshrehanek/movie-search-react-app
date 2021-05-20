f864410f81e4bd0118459123a686f22a406631a2
Josh Rehanek <joshrehanek@gmail.com>
2021-05-20 12:55:13 -0700

    adds if/else to change vote classes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,17 +2,31 @@ import React from 'react';
 
 const imageCall = 'https://image.tmdb.org/t/p/w500';
 
+const setVoteClass = (vote) => {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 6) {
+        return "yellow";
+    } else {
+        return "red"
+    }
+}
+
 
 const Movie = ({ title, poster_path, overview, vote_average, release_date }) =>
-    <div className='movie'>
+    <div className="movie">
         <img src={imageCall + poster_path} alt={title} />
         <div className="movie-info">
             <h4>{title}</h4>
-            <span>{vote_average}</span>
+            <span className={
+                `tag ${setVoteClass(vote_average)}`
+                }>
+                {vote_average}
+            </span>
         </div>
 
         <div className="release-date">
-        <h6>Release Date: {release_date}</h6>
+            <h6>Release Date: {release_date}</h6>
         </div>
 
         <div className="movie-overview">
